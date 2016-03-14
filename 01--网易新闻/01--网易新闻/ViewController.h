624493889aa792f84db37978e84d3a4ca7d55ea4
柴瑞军 <alen@alendemacbook-air.local>
2016-03-14 20:10:54 +0800

    初始化项目

---
+++ 01--网易新闻/01--网易新闻/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  01--网易新闻
+//
+//  Created by 柴瑞军 on 16/3/14.
+//  Copyright © 2016年 myCompany. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 01--网易新闻/01--网易新闻/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  01--网易新闻
+//
+//  Created by 柴瑞军 on 16/3/14.
+//  Copyright © 2016年 myCompany. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
